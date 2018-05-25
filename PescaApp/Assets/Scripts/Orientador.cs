db2c69da0b70e816c488a909cee30e117d89729c
Pablo <pablo97.cantarin@gmail.com>
2018-05-25 19:53:35 -0300

    Cree clase orientador
    Clase orientador con sus variables.

---
+++ PescaApp/Assets/Scripts/Orientador.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orientador : MonoBehaviour 
+{
+	public int id;
+	public string name, mail, cellphone;
+
+}
