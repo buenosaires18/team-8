898d4282089a07f6cfa04d85a607c940b5cfebbf
Emiliano <emiliano_g26@hotmail.com>
2018-05-25 20:56:13 -0300

    Agregue las preguntas

---
+++ PescaApp/Assets/Scripts/Quiz.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Quiz : MonoBehaviour
+{
+
+    private GameObject questions;
+    public List<string> answer = new List<string>();
+
+    void Awake()
+    {
+
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            for (int i = 0; i < answer.Count; i++)
+            {
+                print(answer[i]);
+            }
+        }
+    }
+}
---
+++ PescaApp/Assets/Scripts/StringQuestion.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StringQuestion : MonoBehaviour {
+
+    public InputField inputTxt;
+    public Quiz answerContainer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+    public void Enter()
+    {
+        if (inputTxt.text != "")
+        {
+            answerContainer.answer.Add(inputTxt.text);
+            inputTxt.text = "";
+        }
+    }
+}
