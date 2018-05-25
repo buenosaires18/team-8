594c67e0e681757868a6e18fea9c2d8ee68b17b0
Pablo <pablo97.cantarin@gmail.com>
2018-05-25 20:49:42 -0300

    Create orientador constructor
    Changed the sing in windowd

--- PescaApp/Assets/Scripts/Orientador.cs
+++ PescaApp/Assets/Scripts/Orientador.cs
@@ -7,4 +7,12 @@ public class Orientador : MonoBehaviour
 	public int id;
 	public string name, mail, cellphone;
 
+	public Orientador (int id, string name, string mail, string cellphone)
+	{
+		this.id = id;
+		this.name = name;
+		this.mail = mail;
+		this.cellphone = cellphone;
+	}
+
 }
