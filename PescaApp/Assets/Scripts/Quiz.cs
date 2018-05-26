de4e060a7dd4d63abf62e781f49fd1aeb5a1bbb8
Emiliano <emiliano_g26@hotmail.com>
2018-05-25 23:09:14 -0300

    prueba 3

---
+++ PescaApp/Assets/Scripts/Quiz.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Quiz : MonoBehaviour
+{
+
+    public GameObject questionsContainer;
+    [HideInInspector]
+    public List<string> answer = new List<string>();
+    [HideInInspector]
+    public List<GameObject> questions = new List<GameObject>();
+
+    void Awake()
+    {
+        for (int i = 0; i < questionsContainer.GetComponentInChildren<Transform>().childCount; i++)
+        {
+            questions.Add(questionsContainer.GetComponentInChildren<Transform>().GetChild(i).gameObject);
+        }
+        questions[0].SetActive(true);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            for (int i = 0; i < answer.Count; i++)
+            {
+                print(answer[i]);
+            }
+        }
+    }
+}
