85013b93b1f3857a8e99e30da5f3f530ef92aeb5
RamaSpita <ramuspita@hotmail.com>
2018-05-25 23:26:06 -0300

    JobManager

---
+++ PescaApp/Assets/Scripts/JobManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JobManager : MonoBehaviour {
+
+    public List<Job> jobs;
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	
+    public void Activate()
+    {
+        for (int i = 0; i < jobs.Count; i++)
+        {
+            jobs[i].CheckIfShow();
+        }
+    }
+}
