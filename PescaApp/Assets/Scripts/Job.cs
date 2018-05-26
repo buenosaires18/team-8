346144e8ba3cbdba7570ef363a8ef95a395e9668
RamaSpita <ramuspita@hotmail.com>
2018-05-25 22:37:10 -0300

    Add Jobs

---
+++ PescaApp/Assets/Scripts/Job.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Job
+{
+    public List<Interests> neededInterests;
+    public string jobName;
+    public string jobPosition;
+	
+}
