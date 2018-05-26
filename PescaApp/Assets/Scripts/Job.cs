400a1fe5d80f21e42acd61ab822711dcc35de124
RamaSpita <ramuspita@hotmail.com>
2018-05-25 23:24:27 -0300

    s

--- PescaApp/Assets/Scripts/Job.cs
+++ PescaApp/Assets/Scripts/Job.cs
@@ -8,12 +8,39 @@ public class Job : MonoBehaviour
     public string jobName;
     public string jobPosition;
     public bool active;
+    public static User actuaUser;
+    public DataBase dB;
 
     void Start()
     {
         this.gameObject.SetActive(active);
+        dB = GameObject.Find("DataBase").GetComponent<DataBase>();
     }
-
+    public void CheckIfShow()
+    {
+        
+        if (actuaUser.type == TypeUser.Student)
+        {
+            int i=0;
+            int j = 0;
+            bool check = true;
+            while (i<neededInterests.Count && check)
+            {
+                j = 0;
+                while (j < dB.studentList[actuaUser.id].interests.Count && check)
+                {
+                    if (dB.studentList[actuaUser.id].interests[j] == neededInterests[i])
+                    {
+                        check = false;
+                        return;
+                    }
+                    j++;
+                }
+                i++;
+            }
+        }
+        this.gameObject.SetActive(false);
+    }
 
 
 	
